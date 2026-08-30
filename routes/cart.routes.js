71d883021e39d2547109c64f2a3dd9e8562bd86d
YoussefNakhla <youssf.attia@gmail.com>
2026-08-30 10:19:35 +0300

    feat: set up initial server and routing structure for the book store API

---
+++ routes/cart.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+// Cart routes will be implemented here
+
+module.exports = router;
---
+++ routes/category.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+// Category routes will be implemented here
+
+module.exports = router;
